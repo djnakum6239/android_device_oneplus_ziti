d88db009bb5b0095f5e9cf27b45db9be2e351537
djnakum6239 <d.j.nakum6239@gmail.com>
2026-09-10 07:44:53 +0530

    Clean up AndroidProducts.mk file
    Removed copyright notices and product makefile entries.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,5 @@
-#
-# Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
+LOCAL_PATH := $(call my-dir)
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_ziti.mk
-
-COMMON_LUNCH_CHOICES := \
-    twrp_ziti-user \
-    twrp_ziti-userdebug \
-    twrp_ziti-eng
+ifeq ($(TARGET_DEVICE),ziti)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+endif
