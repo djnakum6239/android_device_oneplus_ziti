9ea4d4f5ed5fdeeb9445206afaf505434e22a4ff
djnakum6239 <d.j.nakum6239@gmail.com>
2026-09-10 09:16:53 +0530

    Refactor PRODUCT_PACKAGES for system tools and A/B updates

--- device.mk
+++ device.mk
@@ -7,19 +7,16 @@ AB_OTA_POSTINSTALL_CONFIG += \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
-# Boot control components needed for A/B slot swapping in TWRP
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service \
-    bootctrl.sm7325
-
+# Core packaging required for system tools and A/B updates
 PRODUCT_PACKAGES += \
     otapreopt_script \
     update_engine \
     update_verifier \
     update_engine_sideload
+
+# Boot control components needed for A/B slot swapping in TWRP
+PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
     android.hardware.boot@1.0-service \
     bootctrl.sm7325 \
     bootctrl.sm7325.recovery
-
