545eeabac13a0882af0b5186e2b0b7a786d5442b
djnakum6239 <d.j.nakum6239@gmail.com>
2026-09-11 19:10:06 +0530

    Initialize product build variables in twrp_ziti.mk
    Added product initialization variables for boot and recovery images.

--- twrp_ziti.mk
+++ twrp_ziti.mk
@@ -3,6 +3,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 $(call inherit-product, device/oneplus/ziti/device.mk)
 
+# Direct product initialization variables
+PRODUCT_BUILD_BOOT_IMAGE := true
+PRODUCT_BUILD_VENDOR_BOOT_IMAGE := false
+PRODUCT_BUILD_RECOVERY_IMAGE := false
+
 PRODUCT_DEVICE := ziti
 PRODUCT_NAME := twrp_ziti
 PRODUCT_BRAND := OnePlus
